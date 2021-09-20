58ea4cdec3e3cb5c5a086a4c79b562b65df059f5
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2021-09-20 10:31:52 +0200

    Added Steam player count getter route
    Route "get_player_count" will return the amount of players currently playing Starbase. Steam API key seems to be optional.

--- Project/index/views.py
+++ Project/index/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import requests
+import os
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse(render(request, 'index/index.html'))
+
+
+def get_player_count(request):
+    api_key = os.getenv('STEAM_API_KEY', '')
+    app_id = 454120
+
+    starbase_player_count_url = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key={api_key}&format=json&appid={app_id}"
+    player_count_request = requests.get(starbase_player_count_url)
+    return JsonResponse({'player_count': player_count_request.json()['response']['player_count']})
