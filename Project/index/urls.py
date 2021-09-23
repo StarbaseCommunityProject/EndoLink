f721b71e4e3a15512a0c2525f091c6de2591bd88
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2021-09-23 23:21:56 +0200

    Added starbase news route
    You can fetch the latest Starbase Steam news using this new route.

--- Project/index/urls.py
+++ Project/index/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('get_player_count', views.GetPlayerCountView.as_view(), name='get_player_count'),
+    path('get_starbase_news', views.GetStarbaseNewsView.as_view(), name='get_starbase_news'),
 ]
--- Project/index/views.py
+++ Project/index/views.py
@@ -15,7 +15,7 @@ def index(request):
 class GetPlayerCountView(View):
     # TODO: Potentially log player count to get historical Starbase data?
     cached_player_count = 0
-    time_since_player_count_request = time.time() - 0   # Prevents an unlikely call within the first 5 seconds of the site being up from showing 0 users.
+    time_since_player_count_request = time.time() - 5   # Prevents an unlikely call within the first 5 seconds of the site being up from showing 0 users.
 
     @classmethod
     def get(cls, request):
@@ -33,3 +33,25 @@ class GetPlayerCountView(View):
             cls.cached_player_count = player_count_request.json()['response']['player_count']
 
         return JsonResponse({'player_count': cls.cached_player_count, 'cached': cached})
+
+
+class GetStarbaseNewsView(View):
+    cached_news = ""
+    time_since_news_request = time.time() - 1800   # Prevents a call within the first 30 minutes of the site being up from showing no news.
+
+    @classmethod
+    def get(cls, request):
+        cached = True
+        if time.time() - cls.time_since_news_request >= 1800:
+            cls.time_since_news_request = time.time()
+
+            cached = False
+
+            api_key = os.getenv('STEAM_API_KEY', '')
+            app_id = 454120
+
+            starbase_news_url = f"https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?key={api_key}&format=json&appid={app_id}"
+            starbase_news_request = requests.get(starbase_news_url)
+            cls.cached_news = starbase_news_request.json()["appnews"]["newsitems"][-1]
+
+        return JsonResponse({'news': cls.cached_news, 'cached': cached})
