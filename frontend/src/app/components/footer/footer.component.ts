454d6f0eb212dccf45973fcd48b5f8b422242f3a
Pascal Masson <pascal220900@gmail.com>
2021-10-08 19:22:08 +0200

    Changed /aboutus to /about
    replaced /aboutus link with /about

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  footerItems = [{name: "About us", link: "/aboutus"}, {name: "Cookie Policy", link: "/cookies"}, {name: "Privacy Policy", link: "/privacy"}]
+  footerItems = [{name: "About us", link: "/about"}, {name: "Cookie Policy", link: "/cookies"}, {name: "Privacy Policy", link: "/privacy"}]
 
   constructor() { }
 
