5dc92e385c778bc77d2a20d607419de1896c73e4
Pascal Masson <pascal220900@gmail.com>
2021-10-08 19:04:00 +0200

    Footer
    add a footer using flexbox, generates dynamically from an array in the component ts file

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LandingComponent } from './pages/landing/landing.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     NavigationComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  footerItems = [{name: "About us", link: "/aboutus"}, {name: "Cookie Policy", link: "/cookies"}, {name: "Privacy Policy", link: "/privacy"}]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
