497b5e0cc03f90c54a49ccd601e93eb756de741d
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2021-10-09 22:09:10 +0200

    Set up multiple settings files
    We can now switch between development & production settings through the DJANGO_SETTINGS_MODULE variable in docker-compose.yml.
    
    The default is currently set to development settings.

---
+++ backend/settings/development.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-9sp3328n!ha1ico2w@p1e7e5h@e7(p&_rk$2v3_4e=n=t0qwqk'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+REST_FRAMEWORK['DEFAULT_THROTTLE_RATES']['anon'] = '1000/minute'
+REST_FRAMEWORK['DEFAULT_THROTTLE_RATES']['user'] = '1000/minute'
+REST_FRAMEWORK['DEFAULT_THROTTLE_RATES']['register'] = '1000/minute'
+
+SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] = timedelta(days=100)
+SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'] = timedelta(days=100)
+SIMPLE_JWT['SIGNING_KEY'] = SECRET_KEY
---
+++ backend/settings/production.py
@@ -0,0 +1,11 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+SIMPLE_JWT['SIGNING_KEY'] = SECRET_KEY
+
+ALLOWED_HOSTS.append("www.endo.link/")
