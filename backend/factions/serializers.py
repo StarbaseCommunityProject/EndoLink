2c514763302f5f9e4b2692475c0e432a18669044
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2021-10-10 16:04:25 +0200

    Add API route for faction creation by a user

--- backend/factions/serializers.py
+++ backend/factions/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class FactionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Faction
-        fields = ['url', 'id', 'name', 'description', 'tags', 'emblem', 'advertisement', 'leader', 'members', 'invitations', 'roles', 'created_at']
+        fields = ['url', 'id', 'name', 'faction_tag', 'description', 'tags', 'emblem', 'advertisement', 'leader', 'members', 'invitations', 'roles', 'created_at']
 
 
 class FactionMemberSerializer(serializers.HyperlinkedModelSerializer):
@@ -30,3 +30,18 @@ class FactionAdvertisementSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = FactionAdvertisement
         fields = ['url', 'id', 'image', 'description', 'message']
+
+
+class FactionCreationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Faction
+        fields = ['name', 'faction_tag', 'description', 'tags', 'emblem']
+
+    def save(self, **kwargs):
+        new_faction = Faction(name=self.validated_data['name'], faction_tag=self.validated_data['faction_tag'], description=self.validated_data['description'], tags=self.validated_data['tags'], emblem=self.validated_data['emblem'], leader=self.validated_data['leader'])
+        new_faction.save()
+
+        new_faction_member = FactionMember(user=self.validated_data['leader'], faction=new_faction)
+        new_faction_member.save()
+
+        return new_faction
