145da5a07e0662a3a6a00e3129ceff1594991974
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2021-10-11 20:30:33 +0200

    Fix for simplejwt issue 266
    Users with outstanding tokens cannot be deleted if the simplejwt token blacklist app is enabled. This is because outstanding tokens override admin permissions. This fix overrides that and gives admins permission to delete outstanding tokens (and as such; permission to delete users with outstanding tokens).
    
    More info: https://github.com/jazzband/djangorestframework-simplejwt/issues/266

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import UserExtraInfo
+from rest_framework_simplejwt import token_blacklist
+
 
 # Register your models here.
 
@@ -9,3 +11,15 @@ class UserExtraInfoAdmin(admin.ModelAdmin):
 
 
 admin.site.register(UserExtraInfo, UserExtraInfoAdmin)
+
+
+# The following is a fix for an issue with the rest_framework_simplejwt blacklist app.
+# As per: https://github.com/jazzband/djangorestframework-simplejwt/issues/266
+class OutstandingTokenAdmin(token_blacklist.admin.OutstandingTokenAdmin):
+
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+
+
+admin.site.unregister(token_blacklist.models.OutstandingToken)
+admin.site.register(token_blacklist.models.OutstandingToken, OutstandingTokenAdmin)
